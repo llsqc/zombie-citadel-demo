a1b8f4c6f2fc1a58e2498bb0ef69a97f0b5ce612
isolation <isolation0601@gmail.com>
2025-08-05 18:27:04 +0800

    feat: 出怪点逻辑

---
+++ Assets/Scripts/GameScene/Object/MonsterPoint.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonsterPoint : MonoBehaviour
+{
+    public int maxWave;
+    public int numEachWave;
+    private int _nowNum;
+
+    public List<int> monsterIds;
+    private int _nowIds;
+
+    public float createOffsetTime;
+
+    public float delayTime;
+
+    public float firstDelayTime;
+
+    private void Start()
+    {
+        Invoke(nameof(CreateWave), firstDelayTime);
+    }
+
+    private void CreateWave()
+    {
+        _nowIds = monsterIds[Random.Range(0, monsterIds.Count)];
+        _nowNum = numEachWave;
+        CreateMonster();
+        --maxWave;
+    }
+
+    private void CreateMonster()
+    {
+        MonsterInfo info = GameDataMgr.Instance.monsterInfoList[_nowIds - 1];
+        GameObject obj = Instantiate(Resources.Load<GameObject>(info.res), transform.position, Quaternion.identity);
+        MonsterObject monsterObj = obj.AddComponent<MonsterObject>();
+        monsterObj.InitInfo(info);
+
+        _nowNum--;
+        if (_nowNum == 0)
+        {
+            if (maxWave > 0)
+            {
+                Invoke(nameof(CreateWave), delayTime);
+            }
+        }
+        else
+        {
+            Invoke(nameof(CreateMonster), createOffsetTime);
+        }
+    }
+
+    public bool IsOver()
+    {
+        return _nowNum == 0 && maxWave == 0;
+    }
+}
