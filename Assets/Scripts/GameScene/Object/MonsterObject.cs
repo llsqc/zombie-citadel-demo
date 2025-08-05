7b6c4aba0f0a3d973c4340fc575e06c573fd97ef
isolation <isolation0601@gmail.com>
2025-08-05 17:32:18 +0800

    feat: 怪物逻辑处理

---
+++ Assets/Scripts/GameScene/Object/MonsterObject.cs
@@ -0,0 +1,92 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class MonsterObject : MonoBehaviour
+{
+    private static readonly int wound = Animator.StringToHash("Wound");
+    private static readonly int dead = Animator.StringToHash("Dead");
+    private static readonly int run = Animator.StringToHash("Run");
+    private static readonly int atk = Animator.StringToHash("Atk");
+    private Animator _animator;
+    private NavMeshAgent _agent;
+    private MonsterInfo _monsterInfo;
+
+    private int hp;
+    public bool isDead = false;
+
+    private float _frontTime;
+
+    private void Awake()
+    {
+        _animator = GetComponent<Animator>();
+        _agent = GetComponent<NavMeshAgent>();
+        _agent.stoppingDistance = 5.0f;
+    }
+
+    public void InitInfo(MonsterInfo info)
+    {
+        _monsterInfo = info;
+        _animator.runtimeAnimatorController = Resources.Load<RuntimeAnimatorController>(info.animator);
+        hp = info.hp;
+        _agent.speed = _agent.acceleration = info.moveSpeed;
+        _agent.angularSpeed = info.roundSpeed;
+    }
+
+    public void Wound(int dmg)
+    {
+        hp -= dmg;
+        _animator.SetTrigger(wound);
+        if (hp <= 0)
+        {
+        }
+        else
+        {
+        }
+    }
+
+    public void Dead()
+    {
+        isDead = true;
+        _agent.isStopped = true;
+        _animator.SetBool(dead, true);
+    }
+
+    public void DeadEvent()
+    {
+    }
+
+    public void BornOver()
+    {
+        _agent.SetDestination(MainTowerObject.Instance.transform.position);
+        _animator.SetBool(run, true);
+    }
+
+    void Update()
+    {
+        if (isDead)
+            return;
+        _animator.SetBool(run, _agent.velocity != Vector3.zero);
+        if( Vector3.Distance(this.transform.position, MainTowerObject.Instance.transform.position ) < 5 &&
+            Time.time - _frontTime >= _monsterInfo.atkOffset)
+        {
+            _frontTime = Time.time;
+            _animator.SetTrigger(atk);
+        }
+    }
+
+    public void AtkEvent()
+    {
+        Collider[] colliders = new Collider[100];
+        var size = Physics.OverlapSphereNonAlloc(transform.position + transform.forward + transform.up, 1, colliders,
+            1 << LayerMask.NameToLayer("MainTower"));
+        for (int i = 0; i < size; i++)
+        {
+            if (MainTowerObject.Instance.gameObject == colliders[i].gameObject)
+            {
+                MainTowerObject.Instance.Wound(_monsterInfo.atk);
+            }
+        }
+    }
+}
